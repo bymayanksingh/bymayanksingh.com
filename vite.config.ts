3745587adefd8f09a63610257ab34509a4862d90
Mayank Singh <mayank.singh081997@gmail.com>
2024-12-12 18:31:07 +0530

    change

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.png', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
         name: 'Professional Software Engineer',
         short_name: 'Portfolio',
